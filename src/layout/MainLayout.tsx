0cad19f22b1910cbd9b9a844b9710cdb5a0eaa20
bader-codes <bader.codes@gmail.com>
2026-09-06 11:48:49 +0300

    fix(navigation): reset scroll position on profile navigation

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import ScrollToTop from "@/components/shared/ScrollToTop";
 import Header from "@/components/header/Header.tsx";
 import { Outlet } from "react-router-dom";
 
@@ -5,6 +6,8 @@ export default function MainLayout() {
   return (
     <>
       <Header />
+
+      <ScrollToTop />
       <main className="pt-20">
         <Outlet />
       </main>
