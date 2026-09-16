96c96f5ec3d304c3b23f327379b7062e08107c07
bader-codes <bader.codes@gmail.com>
2026-09-16 14:43:01 +0300

    Handle expired sessions

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,3 +3,21 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    if (
+      error.response?.status === 401 &&
+      error.response?.data?.message === "jwt expired"
+    ) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      window.location.href = "/login?reason=session-expired";
+    }
+
+    return Promise.reject(error);
+  },
+);
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,46 +3,51 @@ import LoginBanner from "@/components/login/LoginBanner.tsx";
 import Container from "@/components/shared/Container.tsx";
 import LoginForm from "@/components/login/LoginForm.tsx";
 import { Link } from "react-router-dom";
+import ExpiredSection from "@/components/login/ExpiredSection";
 
 export default function Login() {
   return (
-    <section className="py-8 md:py-12">
-      <Container>
-        <div className="mb-10 text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-700 dark:text-gray-50 md:text-5xl">
-            Welcome back to{" "}
-            <span className="bg-linear-to-r from-blue-600 via-sky-500 to-violet-500 bg-clip-text text-transparent">
-              LinkUp
-            </span>
-          </h1>
+    <>
+      <ExpiredSection />
 
-          <p className="mt-3 text-muted-foreground dark:text-gray-100">
-            Log in to your account and continue connecting with your people.
-          </p>
-        </div>
+      <section className="py-8 md:py-12">
+        <Container>
+          <div className="mb-10 text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-700 dark:text-gray-50 md:text-5xl">
+              Welcome back to{" "}
+              <span className="bg-linear-to-r from-blue-600 via-sky-500 to-violet-500 bg-clip-text text-transparent">
+                LinkUp
+              </span>
+            </h1>
 
-        <Card className="overflow-hidden">
-          <CardContent className="grid items-center gap-8 p-4 lg:grid-cols-2 lg:p-6">
-            <div className="hidden lg:block">
-              <LoginBanner />
-            </div>
+            <p className="mt-3 text-muted-foreground dark:text-gray-100">
+              Log in to your account and continue connecting with your people.
+            </p>
+          </div>
 
-            <div className="space-y-6">
-              <LoginForm />
+          <Card className="overflow-hidden">
+            <CardContent className="grid items-center gap-8 p-4 lg:grid-cols-2 lg:p-6">
+              <div className="hidden lg:block">
+                <LoginBanner />
+              </div>
 
-              <p className="font-semibold text-center text-sm text-muted-foreground">
-                Don't have an account?{" "}
-                <Link
-                  to="/signup"
-                  className="font-semibold text-primary transition-colors hover:text-primary/80"
-                >
-                  Sign up
-                </Link>
-              </p>
-            </div>
-          </CardContent>
-        </Card>
-      </Container>
-    </section>
+              <div className="space-y-6">
+                <LoginForm />
+
+                <p className="font-semibold text-center text-sm text-muted-foreground">
+                  Don't have an account?{" "}
+                  <Link
+                    to="/signup"
+                    className="font-semibold text-primary transition-colors hover:text-primary/80"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </Container>
+      </section>
+    </>
   );
 }
